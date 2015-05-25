69fcb364da10ff46f1e5b6d2832503504f886b79
Nick Westman <n2westman@microsoft.com>
2015-05-25 15:47:58 -0700

    Initial

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var tslint = require('gulp-tslint');
+var typescript = require('gulp-tsc');
+var del = require('del');
+
+gulp.task('compile', function () {
+  gulp.src(['ts/**/*.ts'])
+    .pipe(typescript())
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('tslint', function () {
+  gulp.src('ts/**/*.ts')
+    .pipe(tslint({
+      configuration: {
+        "no-any": true,
+        "no-string-literal": true
+      }
+    }))
+    .pipe(tslint.report('verbose'));
+});
+
+gulp.task("default", function () {
+  console.log('running tasks...');
+});
+
+gulp.task("watch", function () {
+  gulp.watch('ts/**/*.ts', function (event) {
+    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+  });
+});
+
+gulp.task('clean', function (cb) {
+  del(['build/*', 'build'], cb);
+});
---
+++ ts/checked.ts
@@ -0,0 +1,33 @@
+// Final Project Examples
+// Nick Westman
+
+module ls {
+	export interface Foo {
+		bar: string;
+		baz?: string;
+	}
+
+	export function Hello(): void {
+		console.log("Hello World");
+
+		var a = new Example();
+	}
+
+	export class Example implements Foo {
+		private x: any = {};
+
+		public bar = "";
+		public baz = "";
+
+		constructor() {
+			this.bar = "Hello";
+			this.baz = "World";
+
+			this["bar"] = "Hello";
+
+			return this;
+		}
+	}
+}
+
+ls.Hello();
